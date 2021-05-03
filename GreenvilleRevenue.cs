021c63e0672d86365685c62c7217ddd7c55ffa16
Mark McFadden <m2web@yahoo.com>
2021-05-03 14:42:59 -0400

    Initial commit

---
+++ GreenvilleRevenue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+using System.Globalization;
+
+public class GreenvilleRevenue
+{
+    public static void Main()
+    {
+        // Write your main here
+    }
+}
